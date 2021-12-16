fafc9668150efd1d6e6aa1b0dc015e68405c0956
Alexey Dogonin <alex.dogonin@gmail.com>
2021-12-17 01:22:46 +0300

    implemented recursive files iteration

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/base64"
 	"errors"
+	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"strconv"
 )
 
@@ -31,57 +33,98 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/media", func(rw http.ResponseWriter, req *http.Request) {
-		request := struct {
-			Cursor string
-		}{}
+	// mux.HandleFunc("/media", func(rw http.ResponseWriter, req *http.Request) {
+	request := struct {
+		Cursor string `json:"cursor"`
+	}{}
 
-		err := json.NewDecoder(req.Body).Decode(&request)
+	// err := json.NewDecoder(req.Body).Decode(&request)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	rw.WriteHeader(http.StatusBadRequest)
+	// 	return
+	// }
+
+	// open dir
+	// iterate from last file
+	// if last file is null, iterate from the begin
+
+	dir := rootPath
+	filename := ""
+	if len(request.Cursor) != 0 {
+		lastFile, err := base64.RawStdEncoding.DecodeString(request.Cursor)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("last file: ", string(lastFile))
+
+		dir, filename = path.Split(string(lastFile))
+		dir = path.Dir(dir)
+	}
+	_ = filename
+	_ = dir
+
+mainLoop:
+	for {
+		dirEntry, err := fs.ReadDir(os.DirFS(dir), ".")
 		if err != nil {
 			log.Println(err)
-			rw.WriteHeader(http.StatusBadRequest)
+			// rw.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
-		// lastFile := base64.RawStdEncoding.DecodeString(request.Cursor)
-		// open dir
-		// iterate from last file
-		// if last file is null, iterate from the begin
+		var e fs.DirEntry
+		found := len(filename) == 0
+		for _, e = range dirEntry {
+			_ = e
+			if !found {
+				if e.Name() == filename {
+					found = true
+				}
+				continue
+			}
 
-		dirEntry, err := fs.ReadDir(os.DirFS(rootPath), "")
-		if err != nil {
-			log.Println(err)
-			rw.WriteHeader(http.StatusInternalServerError)
-			return
-		}
+			if e.IsDir() {
+				dir = path.Join(dir, e.Name())
+				filename = ""
+				continue mainLoop
+			}
 
-		for _, e := range dirEntry {
+			fmt.Println(path.Join(dir, e.Name()))
 			// iterate files and add to resp
 			// last file is cursor
 		}
 
-		resp := []struct {
-			Media []struct {
-				ID string
-				ThumbURL,
-				DetailURL,
-				OriginalURL string
-			}
-			Cursor string
-		}{}
-
-		err = json.NewEncoder(rw).Encode(resp)
-		if err != nil {
-			log.Println(err)
-			rw.WriteHeader(http.StatusInternalServerError)
+		if dir == rootPath {
+			break
 		}
-	})
+
+		dir, filename = path.Split(dir)
+		dir = path.Dir(dir)
+	}
+	// resp := []struct {
+	// 	Media []struct {
+	// 		ID string
+	// 		ThumbURL,
+	// 		DetailURL,
+	// 		OriginalURL string
+	// 	}
+	// 	Cursor string
+	// }{}
+
+	// err = json.NewEncoder(rw).Encode(resp)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	rw.WriteHeader(http.StatusInternalServerError)
+	// }
+	// })
 	mux.HandleFunc("/media/{id}", func(rw http.ResponseWriter, req *http.Request) {
 
 	})
 
-	err := http.ListenAndServe(":"+port, mux)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// err := http.ListenAndServe(":"+port, mux)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
