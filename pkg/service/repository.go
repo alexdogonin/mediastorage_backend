579ebe63d4391bb189eaebdf01721ac5605b22f9
Alexey Dogonin <alex.dogonin@gmail.com>
2022-04-14 00:09:58 +0300

    add syncing into the service

--- pkg/service/repository.go
+++ pkg/service/repository.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	root "github.com/mediastorage_backend/pkg"
 )
@@ -8,8 +10,17 @@ import (
 type Repository interface {
 	Item(uuid.UUID) (root.MediaItem, error)
 	List(cursor string, limit uint) ([]root.MediaItem, string, error)
+	File(path string) (File, error)
+	ListFiles() ([]File, error)
 	Album(UUID uuid.UUID, limit uint, cursor string) (root.MediaAlbum, string, error)
 	UpsertItem(root.MediaItem) error
+	RemoveItem(uuid.UUID) error
 	UpsertAlbum(root.MediaAlbum) error
 	AddItemToAlbum(albumUUID, itemUUID uuid.UUID) error
+}
+
+type File struct {
+	Path      string
+	UpdatedAt time.Time
+	UUID      uuid.UUID
 }
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -20,18 +20,6 @@ func New(repo Repository) Service {
 	return Service{repo}
 }
 
-// TODO необходимо добавить новые файла и альбомы, удалить удалённые
-// идентифицировать файл можно по пути к файлу
-// 1. 
-// пробежаться по директории. если файл есть, то
-//   проверить не изменился ли он. если изменился, то 
-//     обновить сохранив uuid 
-//   иначе
-//     пропустить
-// иначе
-//   добавить новый файл
-// 2. 
-// пробежаться по кэшу. аналогично пункту 1, но теперь смотрим наличие и совпадение файла на жестком диске
 func (s *Service) Fill(rootDir string) error {
 	stat, err := os.Stat(rootDir)
 	if err != nil {
@@ -58,7 +46,7 @@ func (s *Service) Fill(rootDir string) error {
 
 			albums[p] = UUID
 
-			// return s.addAlbum(UUID, e.Name(), baseAlbumUUID)
+			return s.addAlbum(UUID, e.Name(), baseAlbumUUID)
 		}
 
 		return s.addItem(p, baseAlbumUUID)
@@ -125,5 +113,101 @@ func (s *Service) addItem(filePath string, baseAlbum uuid.UUID) error {
 	}
 
 	// return s.repo.AddItemToAlbum(baseAlbum, item.UUID)
+	return nil
+}
+
+// TODO необходимо добавить новые файла и альбомы, удалить удалённые
+// идентифицировать файл можно по пути к файлу
+// 1.
+// пробежаться по директории. если файл есть, то
+//   проверить не изменился ли он. если изменился, то
+//     обновить сохранив uuid
+//   иначе
+//     пропустить
+// иначе
+//   добавить новый файл
+// 2.
+// пробежаться по кэшу. аналогично пункту 1, но теперь смотрим наличие и совпадение файла на жестком диске
+func (s *Service) Sync(rootDir string) error {
+	if err := s.checkCachedData(); err != nil {
+		return err
+	}
+
+	return s.checkDirectoryData(rootDir)
+}
+
+func (s *Service) checkDirectoryData(rootDir string) error {
+	stat, err := os.Stat(rootDir)
+	if err != nil {
+		return err
+	}
+
+	if !stat.IsDir() {
+		return errors.New("rootDir must be a directory")
+	}
+
+	//TODO make stack of directories entry
+	albums := map[string]uuid.UUID{}
+
+	return filepath.WalkDir(rootDir, func(p string, e fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		f, err := s.repo.File(p)
+		if err != nil {
+			return err
+		}
+
+		info, err := e.Info()
+		if err != nil {
+			return err
+		}
+
+		if info.ModTime().Equal(f.UpdatedAt) {
+			return nil
+		}
+
+		d := filepath.Dir(p)
+		baseAlbumUUID := albums[d]
+
+		if e.IsDir() {
+			UUID := uuid.New()
+
+			albums[p] = UUID
+
+			return s.addAlbum(UUID, e.Name(), baseAlbumUUID)
+		}
+
+		return s.addItem(p, baseAlbumUUID)
+	})
+}
+
+func (s *Service) checkCachedData() error {
+	files, err := s.repo.ListFiles()
+	if err != nil {
+		return err
+	}
+
+	for _, p := range files {
+		info, err := os.Stat(p.Path)
+		
+		if err != nil {
+			if err == os.ErrNotExist {
+				err = s.repo.RemoveItem(p.UUID)
+				if err != nil {
+					return err
+				}
+
+			}
+
+			return err
+		}
+
+		if !info.ModTime().Equal(p.UpdatedAt) {
+			// update detail and thumb
+		}
+	}
+
 	return nil
 }
